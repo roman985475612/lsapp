a13397d354d5b5fd68854517c4d38f80bcfba514
Roman <roman985475612@gmail.com>
2020-09-18 09:42:09 +0300

    Changed login views

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
 
-    <title>{{ $title ?? '' }} | {{ config('app.name', 'LSAPP') }}</title>
+    <title>@yield('title'){{ $title ?? '' }} | {{ config('app.name', 'LSAPP') }}</title>
 </head>
 <body>
     @include('inc.navbar')
